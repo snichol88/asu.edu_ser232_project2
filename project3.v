b7171ca821ae72504e32f22ef3a6ab7bfb4a22d4
snichol88 <sjnicho1@asu.edu>
2015-04-25 12:32:34 -0700

    Update project3.v

--- project3.v
+++ project3.v
@@ -20,29 +20,28 @@
 
 module project3(
 	
-
-
 	//declare variables
-   input [15:0] A,
-	input [15:0] B,
+   input signed [15:0] A,
+	input signed [15:0] B,
 	input [3:0] Opcode,
-	output [15:0] print,
-	output ov	
+	output signed [15:0] print,
+	output  ov	
 	);
 	
 	//intiate wire calculations
-	wire [15:0] addition_result = A + B;
-	wire [15:0] subtract_result = A - B;
-	wire [15:0] multiply_result = (A*5);
-	wire [15:0] divide_result = (A/10);
-	wire [15:0] increase_result = (A+1);
-	wire [15:0] decrease_result = (B-1);
-	wire [15:0] bit_and_result = (A & B);
-	wire [15:0] bit_xor_result = (A ^ B);
-	wire [15:0] bit_or_result = (A | B);
-	wire [15:0] complement = ~A;
-	wire [15:0] final;
-	wire overflow;
+	wire signed[15:0] addition_result = A + B;
+	wire signed[15:0] subtract_result = A - B;
+	wire signed[15:0] multiply_result = (A*5);
+	wire signed[15:0] divide_result = (A/10);
+	wire signed[15:0] increase_result = (A+1);
+	wire signed[15:0] decrease_result = (A-1);
+	wire signed[15:0] bit_and_result = (A & B);
+	wire signed[15:0] bit_xor_result = (A ^ B);
+	wire signed [15:0] bit_or_result = (A | B);
+	wire signed[15:0] complement = ~ A;
+	wire signed [15:0] final;
+	wire  overflow; 
+	
 	
 	mux muxer(
 		.addition(addition_result),
@@ -69,56 +68,61 @@ endmodule
 module  mux(
 	
 	//initialize calculations
-	input [15:0] addition,
-	input [15:0] subtract,
-	input [15:0] multiply,
-	input [15:0] divide,
-	input [15:0] increase,
-	input [15:0] decrease,
-	input [15:0] bit_and,
-	input [15:0] bit_xor,
-	input [15:0] bit_or,
-	input [15:0] comp,
+	input signed [15:0] addition,
+	input signed [15:0] subtract,
+	input signed [15:0] multiply,
+	input signed [15:0] divide,
+	input signed [15:0] increase,
+	input signed [15:0] decrease,
+	input signed [15:0] bit_and,
+	input signed [15:0] bit_xor,
+	input signed [15:0] bit_or,
+	input signed [15:0] comp,
 	input [3:0] s,
-	output [15:0] mux_out,
+	output signed [15:0] mux_out,
 	output reg ovf
 	);
 	
-	reg [15:0] mux;
+	reg signed [15:0] mux;
 	assign mux_out = mux;	
 
 	//begin block to choose calculation based upon input s
-	//account for overflow 
+	//account for overflow if larger than 32768 or 1000000000000000
 	always @ (s or addition or subtract or multiply or divide or increase or decrease or bit_and or bit_xor or bit_or or ovf)
 	begin : MUX
+	ovf = 1'b0;
 		//addition if 0 is chosen
 		//accounts for over flow if > 32768 or < -32768
 		if (s == 4'b0000) begin
 			assign mux = addition;			
-			if (addition > 65535 || addition < 0)
+			if (mux > 32768)
 			ovf = 1'b1;
+			else if (mux < -32768)
+			ovf = 1'b1;	
 			else
 			ovf = 1'b0;				
 		end 
 		
+		
 		//subtraction if 1 is chosen
-		//accounts for over flow 
+		//accounts for over flow if > 32768 or < -32768
 		else if(s == 4'b0001) begin
 			assign mux = subtract;
-			if (subtract > 65535 || subtract < 0)
+		if (mux > 32768)
 			ovf = 1'b1;
-			else 
-			ovf = 1'b0;	
-			
+			else if (mux < -32768)
+			ovf = 1'b1;	
+			else
+			ovf = 1'b0;				
 		end 
-		
+				
 		//multiplication if 2 is chosen
-		//accounts for over flow 
+		//accounts for over flow if > 32768 or < -32768
 		else if(s == 4'b0010) begin
 			assign mux = multiply;
-			if (multiply > 65535)
+			if (mux > 32768)
 			ovf = 1'b1;
-			else if (multiply < -65535)
+			else if (mux < -32768)
 			ovf = 1'b1;	
 			else
 			ovf = 1'b0;				
@@ -133,7 +137,7 @@ module  mux(
 		//increase if 4 is chosen
 		else if(s == 4'b0100) begin
 			assign mux = increase;
-			if (increase == 65534)
+			if (mux == 32767)
 			ovf = 1'b1;
 			else
 			ovf = 1'b0;	
@@ -142,11 +146,12 @@ module  mux(
 		//decrease if 5 is chosen
 		else if(s == 4'b0101) begin
 			assign mux = decrease;
-			if (decrease == -65534)
+			if (mux == -32768)
 			ovf = 1'b1;
 			else
-			ovf = 1'b0;	
+			ovf = 1'b0;			
 		end 
+		
 		
 		//bit_and if 6 is chosen
 		else if(s == 4'b0110) begin
